8ffc87e5984385bcd585ba8662522682cef76d62
Christian Bøgh <chri45i4@zbc.dk>
2021-01-04 10:49:27 +0100

    .

---
+++ Tilfaeldigheder/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tilfaeldigheder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+            {
+                //Buffer
+                byte[] data = new byte[4];
+
+                //Ten iterations
+                for (int i = 0; i < 10; i++)
+                {
+                    //Fill buffer
+                    rng.GetBytes(data);
+
+                    //Convert to int
+                    int value = BitConverter.ToInt32(data, 0);
+
+                    //Print value
+                    Console.WriteLine(value);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
